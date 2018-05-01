d7665c6b72ccfe7f897db0ad3e225f00f32ab796
arekkas <aeneas@ory.am>
2018-05-01 23:45:27 +0200

    urlx: AppendPaths handler now properly handles directories

--- urlx/join.go
+++ urlx/join.go
@@ -8,6 +8,16 @@ import (
 // AppendPaths appends the provided paths to the url.
 func AppendPaths(u *url.URL, paths ...string) (ep *url.URL) {
 	ep = Copy(u)
+	if len(paths) == 0 {
+		return ep
+	}
+
 	ep.Path = path.Join(append([]string{ep.Path}, paths...)...)
+
+	last := paths[len(paths)-1]
+	if last[len(last)-1] == '/' {
+		ep.Path = ep.Path + "/"
+	}
+
 	return ep
 }
--- urlx/join_test.go
+++ urlx/join_test.go
@@ -4,9 +4,40 @@ import (
 	"github.com/stretchr/testify/assert"
 	"net/url"
 	"testing"
+	"github.com/stretchr/testify/require"
+	"fmt"
 )
 
 func TestAppendPaths(t *testing.T) {
-	u, _ := url.Parse("https://localhost/1234/")
-	assert.Equal(t, "https://localhost/1234/foo", AppendPaths(u, "/foo").String())
+	u, err := url.Parse("http://localhost/home/")
+	require.NoError(t, err)
+	assert.Equal(t, "http://localhost/home/", AppendPaths(u).String())
+
+	for k, tc := range []struct {
+		give   []string
+		expect string
+	}{
+		{
+			give:   []string{"http://localhost/", "/home"},
+			expect: "http://localhost/home",
+		},
+		{
+			give:   []string{"http://localhost", "/home"},
+			expect: "http://localhost/home",
+		},
+		{
+			give:   []string{"https://localhost/", "/home"},
+			expect: "https://localhost/home",
+		},
+		{
+			give:   []string{"http://localhost/", "/home", "home/", "/home/"},
+			expect: "http://localhost/home/home/home/",
+		},
+	} {
+		t.Run(fmt.Sprintf("case=%d", k), func(t *testing.T) {
+			u, err := url.Parse(tc.give[0])
+			require.NoError(t, err)
+			assert.Equal(t, tc.expect, AppendPaths(u, tc.give[1:]...).String())
+		})
+	}
 }
